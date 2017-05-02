a05d60bd34eb2d09a0dd134825273ba04d54c286
Administrator <Administrator@WIN-TGGDKN2DER3>
2017-05-02 10:53:05 +0530

    commit Chandan

---
+++ AngularJS-MVC/Controllers/DataController.cs
@@ -0,0 +1,34 @@
+﻿using SubSonic.Schema;
+using System.Collections.Generic;
+using System.Data;
+using System.Web.Mvc;
+
+namespace AngularJS_MVC.Controllers
+{
+    public class DataController : Controller
+    {
+        // GET: Data
+        public JsonResult GetSiteMenu()
+        {
+            StoredProcedure sp = new StoredProcedure("GetSiteMenu");
+            DataTable dt = sp.ExecuteDataSet().Tables[0];
+
+            System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
+            Dictionary<string, object> row;
+            foreach (DataRow dr in dt.Rows)
+            {
+                row = new Dictionary<string, object>();
+                foreach (DataColumn col in dt.Columns)
+                {
+                    row.Add(col.ColumnName, dr[col]);
+                }
+                rows.Add(row);
+            }
+
+            return new JsonResult { Data = rows, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+
+        }
+
+    }
+}
